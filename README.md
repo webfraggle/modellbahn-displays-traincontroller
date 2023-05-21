ccf53d50bec92485f9e61198c945aa89415b9159
Christoph Ketzler <christoph@ketzler.de>
2023-05-21 15:59:02 +0200

    Added functions to set train infos directly

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
 # Modellbahn Displays - TrainController Anbindung
+## How to use
+### Option 1: setTime
+Manage all your trains with the webinterface of your "Zugzielanzeiger" and the set the time via command-line / external program with this tool. 
+    
+    mbd-tc-win.exe --setTime "12:30"
+### Option 2: set train infos directly
+ 
+    
+    mbd-tc-win.exe --setTrain1 "ICE123|12:30|Berlin|Hannover - Wolfsburg|0|Kommt von der Commandline" --setTrain2 "RE50|21:12|Bebra|Hünfeld|+10|LOL" --setTrain3 "ICE3|09:45|Lübeck|Hamburg|0|"
+
+## How to develop.
+Please use nvm to switch to node version 18.16
     
     nvm use 18.16
-
-
     npm install
 
 
 
-## Package
-    npm install -g pkg
+## How to build executable.
+I used "pkg" to build Windows & MacOS executables.
 
-    pkg .
+You need the following node modules
+ 
+    npm install -g pkg
+    npm i -g @vercel/ncc
+
+Then you can build it with this command
+
+    ncc build index.js -o build
+    pkg ./build/index.js -t node18-win-arm64,node18-macos-arm64 -o ./dist/mbd-tc
+or 
+    
+    npm run build
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 // first read arguments
 var argv = require('minimist')(process.argv.slice(2));
-console.log(argv);
+// console.log(argv);
 
 // second read config
 
@@ -29,7 +29,7 @@ if (argv['gleis'])
 console.log("Endpoint:", endpoint, "path:", path);
 
 
-// set Time
+// node index.js --setTime "12:30"
 if (argv['setTime'])
 {
     timeString = argv['setTime'];
@@ -45,10 +45,74 @@ if (argv['setTime'])
         time: timeString
     })
     .then(function (response) {
-        console.log('SUCCESS',response);
+        console.log('SUCCESS',response.data);
     })
     .catch(function (error) {
-        console.log('ERROR',error);
+        console.error('ERROR',error);
+        process.exit(1);
+        return;
+        // console.log('ERROR',error.response.status, error.response.data);
+    });
+}
+
+// node index.js --setTrain1 "ICE123|12:30|Berlin|Hannover - Wolfsburg|0|Kommt von der Commandline" --setTrain2 "RE50|21:12|Bebra|Hünfeld|+10|LOL" --setTrain3 "ICE3|09:45|Lübeck|Hamburg|0|"
+
+// --setTrain1 "ICE123|12:30|Berlin|Hannover - Wolfsburg|0|Kommt von der Commandline"
+if (argv['setTrain1'])
+{
+    trainString = argv['setTrain1'];
+    setTrain(1, trainString);
+}
+
+// --setTrain2 "RE50|21:12|Bebra|Hünfeld|+10|LOL"
+if (argv['setTrain2'])
+{
+    trainString = argv['setTrain2'];
+    setTrain(2, trainString);
+}
+
+
+// --setTrain3 "ICE3|09:45|Lübeck|Hamburg|0|"
+if (argv['setTrain3'])
+{
+    trainString = argv['setTrain3'];
+    setTrain(3, trainString);
+}
+
+function setTrain(nr, trainString)
+{
+    outJson = {
+        "vonnach": "",
+        "nr": "",
+        "zeit": "",
+        "via": "",
+        "abw": 0,
+        "hinweis": "",
+        "fusszeile": "",
+        "abschnitte": "",
+        "reihung": "",
+        "path": path
+    }
+    trainInfos = trainString.split("|");
+    outJson.nr = trainInfos[0];
+    outJson.zeit = trainInfos[1];
+    outJson.vonnach = trainInfos[2];
+    outJson.via = trainInfos[3];
+    outJson.abw = trainInfos[4];
+    outJson.hinweis = trainInfos[5];
+
+    // console.log(outJson);
+
+    axios.post(endpoint+'/zug'+nr, outJson)
+    .then(function (response) {
+        console.log('SUCCESS',response.data);
+        process.exit(0);
+        return;
+    })
+    .catch(function (error) {
+        console.error('ERROR',error);
+        process.exit(1);
+        return;
         // console.log('ERROR',error.response.status, error.response.data);
     });
 }
